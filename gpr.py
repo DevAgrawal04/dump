099e817c1229ba2bfed7741d1e7d7b2bd77ac655
Dev Agrawal <devagrawal04@gmail.com>
2023-06-04 22:15:34 +0530

    Create gpr.py

---
+++ gpr.py
@@ -0,0 +1,43 @@
+import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, Matern
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score, KFold
+from hyperopt import hp, fmin, tpe, Trials
+
+# Generate a sample regression dataset
+X, y = generate_sample_data()
+
+# Define the objective function with cross-validation
+def objective(params):
+    kernel = params['kernel'](length_scale=params['length_scale'])
+    model = GaussianProcessRegressor(kernel=kernel, alpha=params['alpha'])
+    kf = KFold(n_splits=5, shuffle=True, random_state=42)
+    scores = -cross_val_score(model, X, y, cv=kf, scoring='neg_mean_squared_error')
+    mse = np.mean(scores)
+    return mse
+
+# Define the search space for hyperparameters
+space = {
+    'kernel': hp.choice('kernel', [
+        {'type': 'rbf', 'kernel': RBF, 'length_scale': hp.loguniform('rbf_length_scale', np.log(0.1), np.log(10))},
+        {'type': 'matern', 'kernel': Matern, 'length_scale': hp.loguniform('matern_length_scale', np.log(0.1), np.log(10)), 'nu': hp.uniform('matern_nu', 0.1, 2)}
+    ]),
+    'alpha': hp.loguniform('alpha', np.log(1e-6), np.log(1e-2))
+}
+
+# Define the optimization function
+def optimize(space):
+    best = fmin(
+        objective,
+        space=space,
+        algo=tpe.suggest,
+        max_evals=100,
+        trials=Trials(),
+        rstate=np.random.RandomState(42)
+    )
+    return best
+
+# Run the optimization
+best_params = optimize(space)
+print("Best Parameters:", best_params)
