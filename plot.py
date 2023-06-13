05b6a96f273ed1007fbee050e542e21b62ab7106
Dev Agrawal <devagrawal04@gmail.com>
2023-06-13 23:44:36 +0530

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,35 +1,53 @@
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+from sklearn.datasets import make_classification
+from xgboost import XGBRegressor
 
 # Create a dummy dataset with 10 features
-np.random.seed(42)
-data = np.random.randn(1000, 10)
-df = pd.DataFrame(data, columns=['feature_'+str(i+1) for i in range(10)])
+X, _ = make_classification(n_samples=1000, n_features=10, random_state=42)
+df = pd.DataFrame(X, columns=[f'feature_{i}' for i in range(10)])
 
 # Select specific indices
 indices = [4, 10, 530, 774]
-df_selected = df.iloc[indices]
+selected_df = df.loc[indices]
 
-# Duplicate rows in each dataframe
-df_selected = pd.concat([df_selected] * 1000, ignore_index=True)
+# Duplicate rows 1000 times
+selected_df = pd.concat([selected_df] * 1000, ignore_index=True)
 
-# Change feature_4 values
-df_selected['feature_4'] = np.linspace(-2, 2, num=len(df_selected))
+# Add values to feature_4
+selected_df['feature_4'] = np.linspace(-2, 2, num=len(selected_df))
+
+# Initialize the XGBoost regressor
+xgb_r = XGBRegressor()
+
+# Make predictions for each dataframe
+for i in range(len(selected_df)):
+    x = selected_df.iloc[[i]]
+    selected_df.loc[[i], 'prediction'] = xgb_r.predict(x)
 
 # Create a plot
 fig = go.Figure()
 
-# Add scatter plots for each dataframe
-for i in range(len(indices)):
-    fig.add_trace(go.Scatter(x=df_selected.index, y=df_selected['feature_2'],
-                             mode='markers', name='Index '+str(indices[i])))
+# Add scatter traces for each dataframe
+for i in range(len(selected_df)):
+    trace = go.Scatter(
+        x=selected_df.loc[i, 'feature_4'],
+        y=selected_df.loc[i, 'prediction'],
+        mode='markers',
+        name=f'Dataframe {i}'
+    )
+    fig.add_trace(trace)
 
-# Set plot layout and labels
-fig.update_layout(title='Feature 2 vs. Feature 4',
-                  xaxis_title='Index',
-                  yaxis_title='Feature 2',
-                  legend_title='Dataframes')
+# Set axis labels and title
+fig.update_layout(
+    xaxis_title='Feature 4',
+    yaxis_title='Predictions',
+    title='Predictions vs Feature 4'
+)
 
-# Show the plot
+# Show the legend
+fig.update_layout(showlegend=True)
+
+# Display the plot
 fig.show()
