6f3c15f35f95ea7e62d9b676b8d509fc06ab1cd6
Dev Agrawal <devagrawal04@gmail.com>
2023-06-14 00:33:16 +0530

    Update plot2.py

--- plot2.py
+++ plot2.py
@@ -1,3 +1,41 @@
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+from sklearn.datasets import make_classification
+from xgboost import XGBRegressor
+
+# Create a dummy dataset with 10 features
+X, _ = make_classification(n_samples=1000, n_features=10, random_state=42)
+df = pd.DataFrame(X, columns=[f'feature_{i}' for i in range(10)])
+
+# Select specific indices
+indices = [4, 10, 530, 774]
+selected_df = df.loc[indices]
+
+# Duplicate rows 1000 times
+selected_df = pd.concat([selected_df] * 1000, ignore_index=True)
+
+# Add values to feature_4
+selected_df['feature_4'] = np.linspace(-2, 2, num=len(selected_df))
+
+# Initialize the XGBoost regressor
+xgb_r = XGBRegressor()
+
+# Store predictions separately
+predictions = []
+
+# Make predictions for each dataframe
+for i in range(len(selected_df)):
+    x = selected_df.iloc[[i]]
+    prediction = xgb_r.predict(x)[0]
+    predictions.append(prediction)
+
+# Add predictions as a new column to each dataframe
+selected_df['prediction'] = predictions
+
+# Get unique dataframe labels
+dataframe_labels = selected_df.index // 1000
+
 # Convert feature_4 and predictions to lists
 x_values = selected_df['feature_4'].tolist()
 y_values = selected_df['prediction'].tolist()
@@ -17,6 +55,19 @@ for label in set(dataframe_labels):
         x=x,
         y=y,
         mode='markers',
-        name=name
+        name=name,
+        legendgroup=name,  # Assign same legend group to link legend and scatter trace
+        showlegend=True,  # Show legend entry for each scatter trace
+        legendrank=label,  # Specify legend rank to control order of legend entries
     )
     fig.add_trace(trace)
+
+# Set axis labels and title
+fig.update_layout(
+    xaxis_title='Feature 4',
+    yaxis_title='Predictions',
+    title='Predictions vs Feature 4'
+)
+
+# Display the plot
+fig.show()
