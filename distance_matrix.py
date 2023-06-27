048f131d3175005b41df07009ac0cd76534e4665
Dev Agrawal <devagrawal04@gmail.com>
2023-06-27 23:06:10 +0530

    Update distance_matrix.py

--- distance_matrix.py
+++ distance_matrix.py
@@ -14,11 +14,13 @@ threshold = 0.5  # Adjust this value as per your requirement
 
 # Find the indices of data points to be removed
 indices_to_remove = []
+removed_indices = set()  # To keep track of the removed indices
 for i, row in enumerate(distance_matrix):
-    # Exclude the diagonal element
-    close_indices = [j for j, d in enumerate(row) if j != i and d < threshold]
+    # Exclude the diagonal element and already removed indices
+    close_indices = [j for j, d in enumerate(row) if j != i and d < threshold and j not in removed_indices]
     if close_indices:
-        indices_to_remove.extend(close_indices)
+        indices_to_remove.append(close_indices[0])  # Remove only the first close point
+        removed_indices.add(close_indices[0])  # Add the removed index to the set
 
 # Remove the data points from the dataframe
 df = df.drop(df.index[indices_to_remove])
