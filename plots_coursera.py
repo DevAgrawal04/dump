2e8421cb19271d3e16dfa5ff058fead0b8355078
Dev Agrawal <devagrawal04@gmail.com>
2023-09-02 15:39:25 +0530

    Update plots_coursera.py

--- plots_coursera.py
+++ plots_coursera.py
@@ -1,4 +1,4 @@
-#classification X and O
+## Classification X and O
 fig,ax = plt.subplots(1,1,figsize=(4,4))
 plot_data(X, y, ax)
 
@@ -7,7 +7,8 @@ ax.set_ylabel('$x_1$')
 ax.set_xlabel('$x_0$')
 plt.show()
 
-#Plot a sigmoid and make a vertical threshold boundary with colours
+
+## Plot a sigmoid and make a vertical threshold boundary with colours
 # Plot sigmoid(z) over a range of values from -10 to 10
 z = np.arange(-10,11)
 
@@ -22,7 +23,7 @@ draw_vthresh(ax,0)
 
 
 
-#Make a -45 degree line and shade the area under the line
+## Make a -45 degree line and shade the area under the line
 # Choose values between 0 and 6
 x0 = np.arange(0,6)
 
@@ -40,3 +41,28 @@ plot_data(X,y,ax)
 ax.set_ylabel(r'$x_1$')
 ax.set_xlabel(r'$x_0$')
 plt.show()
+
+
+## Multiple Decision Plots
+x0 = np.arange(0,6)
+
+# Plot the two decision boundaries
+x1 = 3 - x0
+x1_other = 4 - x0
+
+fig,ax = plt.subplots(1, 1, figsize=(4,4))
+# Plot the decision boundary
+ax.plot(x0,x1, c=dlc["dlblue"], label="$b$=-3")
+ax.plot(x0,x1_other, c=dlc["dlmagenta"], label="$b$=-4")
+ax.axis([0, 4, 0, 4])
+
+# Plot the original data
+plot_data(X_train,y_train,ax)
+ax.axis([0, 4, 0, 4])
+ax.set_ylabel('$x_1$', fontsize=12)
+ax.set_xlabel('$x_0$', fontsize=12)
+plt.legend(loc="upper right")
+plt.title("Decision Boundary")
+plt.show()
+
+
